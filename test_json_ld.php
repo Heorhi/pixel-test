6ea909ce5667654eae1fb44f3d919932c6645ea2
Heorhi <heorhi.raik@gmail.com>
2020-05-13 22:29:44 +0100

    Update test_json_ld.php

--- test_json_ld.php
+++ test_json_ld.php
@@ -38,8 +38,8 @@
   "productID":"<?php echo_param_or_default("retailer_item_id", "id_01"); ?>",
   "name":"Facebook T-Shirt",
   "description":"Unisex Facebook T-shirt, Small",
-  "url":"<?php echo "http://fb-pixel-test.herokuapp.com".$_SERVER['REQUEST_URI']; ?>",
-  "image":"<?php echo "http://fb-pixel-test.herokuapp.com".$_SERVER['REQUEST_URI']; ?>",
+  "url":"<?php echo "https://fb-pixel-test.herokuapp.com".$_SERVER['REQUEST_URI']; ?>",
+  "image":"<?php echo "https://fb-pixel-test.herokuapp.com".$_SERVER['REQUEST_URI']; ?>",
   "brand":"facebook",
   "offers": [
     {
